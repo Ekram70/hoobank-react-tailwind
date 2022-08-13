a684d5887099c1f0dd430ece093ba14034797445
Ekram70 <ekramullah70@gmail.com>
2022-08-13 23:05:13 +0600

    hero section done

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Billing from './Billing';
+import Business from './Business';
+import CardDeal from './CardDeal';
+import Clients from './Clients';
+import CTA from './CTA';
+import Footer from './Footer';
+import Hero from './Hero';
+import Navbar from './Navbar';
+import Stats from './Stats';
+import Testimonial from './Testimonials.jsx';
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonial,
+  Hero,
+};
